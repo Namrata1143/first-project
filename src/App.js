7ed1f660d690a8adbcaec58148d925072bb46783
Namrata Pudasainee <pudasaineenamrata@gmail.com>
2022-08-26 12:50:45 +0545

    pass the component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <p> Oi namarate.</p>
+      <p> Oi namarate.</p> <Hello place="London"/>
+      <p> Oi namarate.</p> <Hello place="New York"/>
+      <p> Oi namarate.</p> <Hello place="Nepal"/>
     </div>
   );
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hello.css';
+
+const Hello = ({place}) => {
+  return (
+    <div>
+    <p>Welcome to the {place} show.</p>
+    <p></p>
+    </div>
+  )
+}
+
+export default Hello
